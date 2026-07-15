efe6b2c4591ea5435e478bb1181f6d6635bbcd2e
Artyom Gusev <agusev2311@mail.ru>
2026-07-15 12:31:49 +0300

    very little update

--- work_with_awg/__init__.py
+++ work_with_awg/__init__.py
@@ -5,8 +5,11 @@ from . import generate_server_config
 
 conf = config.get_config()
 
+def config_reload():
+    generate_server_config.apply_new_conf(generate_server_config.generate_server_config())
+
 def new_user_config(owner_id):
     new_conf = new_config.generate_new_config(conf["awg_dir"])
     new_conf_id = configs.insert_new_config(db=conf["db_filename"], free_ip=new_conf[0], public_key=new_conf[1], private_key=new_conf[2], owner_id=owner_id)
-    generate_server_config.apply_new_conf(generate_server_config.generate_server_config())
+    config_reload()
     return new_conf_id
