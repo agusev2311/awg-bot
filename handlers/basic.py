d2e8d5ac023ba4c3d86c38802cabcdcf71600355
Artyom Gusev <agusev2311@mail.ru>
2026-07-21 13:10:31 +0300

    tamp_basic_edit

--- handlers/basic.py
+++ handlers/basic.py
@@ -28,7 +28,13 @@ def register_handlers(bot, db_filename):
     def handle_banned_message(message):
         bot.reply_to(message, "Your account has been banned")
 
-        file_buffer = io.BytesIO(conf.encode("utf-8"))
-        file_buffer.name = f"awg_{new_conf_id}.conf"
+    # @bot.message_handler(commands=["new_config"])
+    # def new_config(message):
+    #     user_id = message.from_user.id
+    #     new_conf_id = work_with_awg.new_user_config(user_id)
+    #     conf = generate_client_config.generate(new_conf_id)
 
-        bot.send_document(chat_id=message.chat.id, document=file_buffer,)
+    #     file_buffer = io.BytesIO(conf.encode("utf-8"))
+    #     file_buffer.name = f"awg_{new_conf_id}.conf"
+
+    #     bot.send_document(chat_id=message.chat.id, document=file_buffer,)
